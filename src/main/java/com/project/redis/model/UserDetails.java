22c3c49b1ba3c7b2905cb41ee51c935b1266f04e
JayaKumar <jayakumar@astiinfotech.com>
2025-06-06 16:18:04 +0530

    changes

--- src/main/java/com/project/redis/model/UserDetails.java
+++ src/main/java/com/project/redis/model/UserDetails.java
@@ -1,7 +1,6 @@
 package com.project.redis.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +10,16 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity
 @Table(name = "user_details")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserDetails implements Serializable {
 
     @Id
@@ -34,8 +37,52 @@ public class UserDetails implements Serializable {
     @JsonIgnore
     @Column(name = "deleted_on")
     private LocalDateTime deletedOn;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public LocalDateTime getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(LocalDateTime createdOn) {
+		this.createdOn = createdOn;
+	}
+	public boolean isDeleted() {
+		return isDeleted;
+	}
+	public void setDeleted(boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	public LocalDateTime getDeletedOn() {
+		return deletedOn;
+	}
+	public void setDeletedOn(LocalDateTime deletedOn) {
+		this.deletedOn = deletedOn;
+	}
 
 //    @Version
 //    private Integer version = 0;
+    
+    
 
 }
